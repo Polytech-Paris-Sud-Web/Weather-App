bfa12f5cb0e1f2fd452e3eb23d94e515754be198
jonathan <jonathanramassamy@outlook.fr>
2020-05-04 16:00:27 +0200

    init repo of weather app - Heroku Project Name: Aer Url: https://aer-weather-app.herokuapp.com/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { ErrorComponent } from './ui/error/error.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { AddComponent } from './pages/add/add.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCardComponent,
+    ErrorComponent,
+    WeatherCardComponent,
+    AddComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
