256ac6df9e20dab4fe10d061803bdbd759de14b3
nelson888 <tambue@hotmail.fr>
2020-05-05 11:17:01 +0200

    created getWeather method

--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {first} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  private readonly weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  private readonly appID = '6cc51d73a359e8cb5411141a9f0cbcca';
+
+  constructor(public http: HttpClient) {
+  }
+
+  getWeather(city: string): Observable<any> {
+    return this.http.get(
+      `${this.weatherURL}${city}&APPID=${this.appID}`).pipe((first()));
+  }
 }
