a94326289a3e55dcd1ecd6aa04ced8492b30e96c
nelson888 <tambue@hotmail.fr>
2020-05-05 12:34:15 +0200

    added get forecast endpoint

--- src/app/model/weather.ts
+++ src/app/model/weather.ts
@@ -4,3 +4,29 @@ type WeatherResponse = {
   description: string;
   icon: string;
 };
+
+type ForecastResponse = {
+  dt: number,
+  main: {
+    temp: number,
+    temp_min: number,
+    temp_max: number,
+    pressure: number,
+    sea_level: number,
+    grnd_level: number,
+    humidity: number,
+    temp_kf: number
+  },
+  weather: WeatherResponse[],
+  clouds: {
+    all: number
+  },
+  wind: {
+    speed: number,
+    deg: number
+  },
+  sys: {
+    pod: string
+  },
+  dt_text: string
+};
--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -9,6 +9,7 @@ import {first, map} from 'rxjs/operators';
 export class WeatherService {
 
   private readonly weatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+  private readonly forecastURL = 'https://api.openweathermap.org/data/2.5/forecast';
 
   private readonly appID = '6cc51d73a359e8cb5411141a9f0cbcca';
 
@@ -17,7 +18,12 @@ export class WeatherService {
 
   getWeather(city: string): Observable<WeatherResponse> {
     return this.http.get(`${this.weatherURL}?q=${city}&APPID=${this.appID}`)
-      .pipe((first()))
-      .pipe(map((payload: any) => payload.weather[0]));
+      .pipe(first(), map((payload: any) => payload.weather[0]));
+  }
+
+  getForecast(city: string): Observable<ForecastResponse[]> {
+    return this.http.get(
+      `${this.forecastURL}?q=${city}&APPID=${this.appID}`)
+      .pipe(first(), map((weather: any) => weather.list));
   }
 }
